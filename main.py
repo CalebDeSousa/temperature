1528f48d562fcde9e3cb8749bf59ffc7690439e3
CalebDeSousa <71708632+CalebDeSousa@users.noreply.github.com>
2020-11-13 23:44:31 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+(temperature) = input.temperature()
+
+if (temperature < 10):
+    basic.show_string("COLD")
+elif (temperature >=10):
+    basic.show_string("WARM!")
---
+++ main.ts
@@ -0,0 +1,7 @@
+let temperature = input.temperature()
+if (temperature < 10) {
+    basic.showString("COLD")
+} else if (temperature >= 10) {
+    basic.showString("WARM!")
+}
+
